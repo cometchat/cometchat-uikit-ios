afddc089339e526dc5c9a94097c25e969fc9658d
Pushpsen Airekar <pushpsen.airekar@cometchat.com>
2022-02-10 20:02:55 +0530

    added load view

--- Sources/CometChatWorkspace/Components/dummy.swift
+++ Sources/CometChatWorkspace/Components/dummy.swift
@@ -15,15 +15,15 @@ public class dummy: UIViewController {
        
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    public override func loadView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "dummy", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view  = view
     }
-    */
+   
+
 
 }
