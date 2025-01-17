8cd1f61ee711a9933c09439c5fb317ffd0f373bc
Dawinder Kaur <dawinder.kaur@cometchat.com>
2025-01-17 22:19:35 +0530

    updated sdk and uikit versions

--- SampleApp/AppConstants.swift
+++ SampleApp/AppConstants.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class AppConstants {
-    static var APP_ID = ""
-    static var AUTH_KEY = ""
-    static var REGION = ""
+    static var APP_ID = "26580020f03ff346"
+    static var AUTH_KEY = "4152b0366478871f0fa8d19a287dd6f5ed5f8eff"
+    static var REGION = "in"
 }
