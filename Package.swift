822d8546f10933ee9e5e967bed0423acda1e3637
Jitvar Patil <jitvar.patil@inscripts.com>
2025-11-12 22:12:59 +0530

    v5.1.1

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         // Defining the binary target for CometChatUIKitSwift.
         .binaryTarget(
             name: "CometChatUIKitSwift",
-            url: "https://library.cometchat.io/ios/v4.0/xcode15/CometChatUIKitSwift_5_0_0.xcframework.zip",
-            checksum: "9ad1d7b4ae30e4e4b78a1f3f21a7f7ea89e422aeba892536bd6f7fa3e490167a"
+            url: "https://library.cometchat.io/ios/v5.0/xcode16/CometChatUIKitSwift_5_1_1.xcframework.zip",
+            checksum: "9905bfe733d22a365dc72142f9776888dde566b7d23dc2731839206b710c6878"
         )
     ]
 )
